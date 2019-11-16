0e8b6806fdf50d672de3341b025ae14419574bcc
davidchanho <davidho.crd@gmail.com>
2019-11-16 12:59:46 -0500

    add start to gulp to add two tasks in one

--- README.md
+++ README.md
@@ -5,33 +5,53 @@
 ### HTML
 
 - PUG
-- Bootstrap 4 - styles html
 
 ### CSS
 
 - SASS
+- Bootstrap 4 - css framework
 
 ### Javascript & Node.js
 
+#### Task runner
+
 - Gulp - automates tasks in the command line
+
   - gulp-pug - complies PUG into HTML
   - gulp-sass - complies SASS into CSS
   - gulp-rename - renames files and folder. Used to remove pugs' directory names.
   - gulp-watch - adds watch mode. compiles files every time there is a change
-- Express.js - handles routing
-- Keys.js & dotenv.js - hides keys
-- Mysql.js - handles database
+  - gulp-nodemon - adds nodemon to watch server changes
+
 - cross-env
-- dotenv
+
+#### Database
+
+- Mysql.js - handles database
+- Sequelize
+
+#### Routing
+
+- Express.js - handles routing
+
+#### Linter
+
 - eslint
   - eslint-config-prettier
 - prettier
 
+#### Security
+
+- keys.js - stores keys
+- dotenv - hides keys
+
 ### Testing
+
 - Travis CI
 - chai
   - chai-http
 - mocha
 
 ### Hosting
+
 - Heroku
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var watch = require('gulp-watch');
+var gulp = require('gulp'); // Task runner.
+var pug = require('gulp-pug'); // allows gulp to work with pug files
+var sass = require('gulp-sass'); // allows gulp to work with sass files
+var rename = require('gulp-rename'); // allows gulp to rename files and folders
+var watch = require('gulp-watch'); // allows gulp to be on watch mode
 
+// sets task to convert pug into html
 gulp.task('pug', function() {
 	return gulp
-		.src('./**/template.pug')
+		.src('./**/template.pug') // file location of the pug file
 		.pipe(
 			pug({
-				doctype: 'html',
-				pretty: true,
-      }),
+				doctype: 'html', // file type to be converted to
+				pretty: true, // formats the file after conversion
+			})
 		)
-		.pipe(rename({ basename: 'index', dirname: '' }))
-		.pipe(gulp.dest('./public'));
+		.pipe(rename({ basename: 'index', dirname: '' })) // renames file and directory
+		.pipe(gulp.dest('./public')); // location of where html will be sent after task is done
 });
 
+// sets task to convert sass into css
 gulp.task('sass', function() {
 	return gulp
 		.src('./**/burger_style.sass')
@@ -30,13 +32,33 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('./public/assets/css/'));
 });
 
+// runs several tasks at once
+gulp.task('start', gulp.parallel('pug', 'sass'));
+
+// start our server and listen for changes
+gulp.task('server', function() {
+    // configure nodemon
+    nodemon({
+        // the script to run the app
+        script: 'server.js',
+        // this listens to changes in any of these files/routes and restarts the application
+        watch: ["server.js", "routes/", 'public/*', 'public/*/**'],
+        ext: 'js'
+        // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
+    }).on('restart', () => {
+    gulp.src('server.js')
+      // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
+      .pipe(notify('Running the start tasks and stuff'));
+  });
+});
+
 // gulp.task('watch', function() {
 //   return watch('./**/*.pug', { ignoreInitial: false }).pipe(
 //     gulp.dest('./public')
 //   );
 // });
 
-gulp.task('watch', function() {
-	watch('./**/*.pug', ['pug']).pipe(gulp.dest('./public'));
-	watch('./**/*.sass', ['styles']).pipe(gulp.dest('./public/assets/css/'));
-});
+// gulp.task('watch', function() {
+// 	watch('./**/*.pug', ['pug']).pipe(gulp.dest('./public'));
+// 	watch('./**/*.sass', ['styles']).pipe(gulp.dest('./public/assets/css/'));
+// });
