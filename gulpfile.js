a21e09a09adffaab164b27deaf067f19ba0f1a58
davidchanho <davidho.crd@gmail.com>
2019-11-18 23:28:38 -0500

    add passport.js, modulize pug files, and fixed gulp compile issues and add eslint command

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,64 @@
-var gulp = require('gulp'); // Task runner.
-var pug = require('gulp-pug'); // allows gulp to work with pug files
-var sass = require('gulp-sass'); // allows gulp to work with sass files
-var rename = require('gulp-rename'); // allows gulp to rename files and folders
-var watch = require('gulp-watch'); // allows gulp to be on watch mode
+var gulp = require("gulp"); // Task runner.
+var pug = require("gulp-pug"); // allows gulp to work with pug files
+var sass = require("gulp-sass"); // allows gulp to work with sass files
+var rename = require("gulp-rename"); // allows gulp to rename files and folders
+var message = require("gulp-message");
+var nodemon = require("gulp-nodemon");
+var eslint = require("gulp-eslint");
 
 // sets task to convert pug into html
-gulp.task('pug', function() {
-	return gulp
-		.src('./**/template.pug') // file location of the pug file
-		.pipe(
-			pug({
-				doctype: 'html', // file type to be converted to
-				pretty: true, // formats the file after conversion
-			})
-		)
-		.pipe(rename({ basename: 'index', dirname: '' })) // renames file and directory
-		.pipe(gulp.dest('./public')); // location of where html will be sent after task is done
+gulp.task("pug", function() {
+  return gulp
+    .src("./views/*.pug") // file location of the pug file
+    .pipe(
+      pug({
+        doctype: "html", // file type to be converted to
+        pretty: true // formats the file after conversion
+      })
+    )
+    .pipe(rename({ dirname: "" })) // renames file and directory
+    .pipe(gulp.dest("./public")); // location of where html will be sent after task is done
 });
 
 // sets task to convert sass into css
-gulp.task('sass', function() {
-	return gulp
-		.src('./**/burger_style.sass')
-		.pipe(
-			sass({
-				doctype: 'css',
-				pretty: true,
-			})
-		)
-		.pipe(rename({ dirname: '' }))
-		.pipe(gulp.dest('./public/assets/css/'));
+gulp.task("sass", function() {
+  return gulp
+    .src("./public/assets/sass/*.sass")
+    .pipe(
+      sass({
+        doctype: "css",
+        pretty: true
+      })
+    )
+    .pipe(rename({ dirname: "" }))
+    .pipe(gulp.dest("./public/assets/css"));
 });
-
+gulp.task('format', function() {
+	return gulp
+		.src(['./public/assets/javascript/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
 // runs several tasks at once
-gulp.task('start', gulp.parallel('pug', 'sass'));
+gulp.task("start", gulp.series("pug", "sass", "format"));
 
-// start our server and listen for changes
-gulp.task('server', function() {
-    // configure nodemon
-    nodemon({
-        // the script to run the app
-        script: 'server.js',
-        // this listens to changes in any of these files/routes and restarts the application
-        watch: ["server.js", "routes/", 'public/*', 'public/*/**'],
-        ext: 'js'
-        // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
-    }).on('restart', () => {
-    gulp.src('server.js')
-      // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
-      .pipe(notify('Running the start tasks and stuff'));
+gulp.task("server", function() {
+  nodemon({
+    script: "server.js",
+    watch: ["server.js", "./routes/*", "/public/*", "public/*/**"],
+    ext: "js"
+  }).on("restart", () => {
+    gulp.src("server.js");
   });
 });
 
-// gulp.task('watch', function() {
-//   return watch('./**/*.pug', { ignoreInitial: false }).pipe(
-//     gulp.dest('./public')
-//   );
-// });
-
-// gulp.task('watch', function() {
-// 	watch('./**/*.pug', ['pug']).pipe(gulp.dest('./public'));
-// 	watch('./**/*.sass', ['styles']).pipe(gulp.dest('./public/assets/css/'));
-// });
+// format
+// messages
+// spellcheck
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,24 @@
 var db = require("../models");
+var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   // Load example page and pass in an example by id
@@ -22,8 +32,22 @@ module.exports = function(app) {
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+  // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
