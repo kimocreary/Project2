34659299d00eb1a79d34cbfc54bcbf604340ec33
richard mcgirt <richardmcgirt@richards-MBP-2.attlocal.net>
2019-11-27 22:24:55 -0500

    final commit

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,28 @@ CREATE DATABASE userdb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+CREATE SCHEMA project2;
+
+CREATE TABLE project2.`user` ( 
+	user_id              int  NOT NULL  AUTO_INCREMENT,
+	first_name           varchar(100)  NOT NULL  ,
+	last_name            varchar(100)  NOT NULL  ,
+	email                varchar(100)  NOT NULL  ,
+	position             varchar(100)  NOT NULL  ,
+	CONSTRAINT pk_user_user_id PRIMARY KEY ( user_id )
+ ) engine=InnoDB;
+
+CREATE TABLE project2.task ( 
+	task_id              int  NOT NULL  AUTO_INCREMENT,
+	user_id              int    ,
+	task                 longtext  NOT NULL  ,
+	priority             int  NOT NULL DEFAULT 1 ,
+	due_date             date  NOT NULL  ,
+	status               varchar(20)    ,
+	CONSTRAINT pk_task_task_id PRIMARY KEY ( task_id )
+ ) engine=InnoDB;
+
+CREATE INDEX idx_task_user_id ON project2.task ( user_id );
+
+ALTER TABLE project2.task ADD CONSTRAINT fk_task_user FOREIGN KEY ( user_id ) REFERENCES project2.`user`( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
--- models/task.js
+++ models/task.js
@@ -1,38 +1,63 @@
-
-// Creating our Task model
-module.exports = function(sequelize, DataTypes) {
-  var Task = sequelize.define("Task", {
-    // The email cannot be null, and must be a proper email before creation
-    task_description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    task_priority: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      target_date: {
-        type: DataTypes.DATE,
-        allowNull: true        
-      },
-      developer_name: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      developer_notes: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      developer_duedate: {
-        type: DataTypes.DATE,
-        allowNull: true
-      },
-      completion_date: {
-        type: DataTypes.DATE,
-        allowNull: true
-      },
-  }, {
-    timestamps: false
-  });
-  return Task;
-};
+ $("form").submit(function(e){
+        e.preventDefault();
+        var name = $("input[name='name']").val();
+        var email = $("input[name='email']").val();
+        var date = $("input[name='date']").val();
+     
+        $(".data-table tbody").append("<tr data-name='"+name+"' data-email='"+email+"' data-date='"+date+"'><td>"+name+"</td><td>"+email+"</td><td>"+date+"</td><td><button class='btn btn-info btn-xs btn-edit'>Edit</button><button class='btn btn-danger btn-xs btn-delete'>Delete</button></td></tr>");
+    
+        $("input[name='name']").val('');
+        $("input[name='email']").val('');
+        $("input[name='date']").val('');
+    });
+   
+    $("body").on("click", ".btn-delete", function(){
+        $(this).parents("tr").remove();
+    });
+    
+    $("body").on("click", ".btn-edit", function(){
+        var name = $(this).parents("tr").attr('data-name');
+        var email = $(this).parents("tr").attr('data-email');
+        var date = $(this).parents("tr").attr('data-date');
+    
+        $(this).parents("tr").find("td:eq(0)").html('<input name="edit_name" value="'+name+'">');
+        $(this).parents("tr").find("td:eq(1)").html('<input name="edit_email" value="'+email+'">');
+        $(this).parents("tr").find("td:eq(2)").html('<input name="edit_date" value="'+date+'">');
+    
+        $(this).parents("tr").find("td:eq(3)").prepend("<button class='btn btn-info btn-xs btn-update'>Update</button><button class='btn btn-warning btn-xs btn-cancel'>Cancel</button>")
+        $(this).hide();
+    });
+   
+    $("body").on("click", ".btn-cancel", function(){
+        var name = $(this).parents("tr").attr('data-name');
+        var email = $(this).parents("tr").attr('data-email');
+        var date = $(this).parents("tr").attr('data-date');
+    
+        $(this).parents("tr").find("td:eq(0)").text(name);
+        $(this).parents("tr").find("td:eq(1)").text(email);
+        $(this).parents("tr").find("td:eq(2)").text(date);
+   
+        $(this).parents("tr").find(".btn-edit").show();
+        $(this).parents("tr").find(".btn-update").remove();
+        $(this).parents("tr").find(".btn-cancel").remove();
+    });
+   
+    $("body").on("click", ".btn-update", function(){
+        var name = $(this).parents("tr").find("input[name='edit_name']").val();
+        var email = $(this).parents("tr").find("input[name='edit_email']").val();
+        var date = $(this).parents("tr").find("input[name='edit_date']").val();
+    
+    
+        $(this).parents("tr").find("td:eq(0)").text(name);
+        $(this).parents("tr").find("td:eq(1)").text(email);
+        $(this).parents("tr").find("td:eq(2)").text(date);
+     
+        $(this).parents("tr").attr('data-name', name);
+        $(this).parents("tr").attr('data-email', email);
+        $(this).parents("tr").attr('data-date', date);
+    
+        $(this).parents("tr").find(".btn-edit").show();
+        $(this).parents("tr").find(".btn-cancel").remove();
+        $(this).parents("tr").find(".btn-update").remove();
+    });
+    
